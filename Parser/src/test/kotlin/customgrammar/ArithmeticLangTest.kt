0d4ea6add464f50640cabd8ee20dd2dc3b7e3908
AntonAsmirko <LoLkingXXX@gmail.com>
2022-12-26 12:30:45 +0300

    tests

--- Parser/src/test/kotlin/customgrammar/ArithmeticLangTest.kt
+++ Parser/src/test/kotlin/customgrammar/ArithmeticLangTest.kt
@@ -1,8 +1,10 @@
 package customgrammar
 
 import com.google.common.truth.Truth
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 import ru.anton.asmirko.lexer.TokenLexer
 import ru.anton.asmirko.parser.parser.RegexParser
 import ru.anton.asmirko.parser.utils.initGrammarFromString
@@ -36,5 +38,28 @@ class ArithmeticLangTest {
         val expectedUrl = resourceURLLoader<ArithmeticLangTest>(expectedFile)
         val expected = readFromUrl(expectedUrl)
         Truth.assertThat(result).isEqualTo(expected)
+    }
+
+    @ValueSource(
+        strings = [
+            "customgrammar/arithmeticlang/negative/input/test1.txt",
+            "customgrammar/arithmeticlang/negative/input/test2.txt"
+        ]
+    )
+    @ParameterizedTest
+    fun `test negative cases`(inputFile: String) {
+        try {
+            val grammarRawUrl = resourceURLLoader<ArithmeticLangTest>("customgrammar/arithmeticlang/arithmeticlang.txt")
+            val grammarRaw = readFromUrl(grammarRawUrl)
+            val inputUrl = resourceURLLoader<ArithmeticLangTest>(inputFile)
+            val input = readFromUrl(inputUrl)
+            val (grammar, tokens) = initGrammarFromString(grammarRaw)
+            val lexer = TokenLexer(tokens, "$")
+            val parser = RegexParser(grammar, lexer)
+            parser.parse(listOf(input))
+            Assertions.fail()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
--- Parser/src/test/kotlin/customgrammar/RegexTest.kt
+++ Parser/src/test/kotlin/customgrammar/RegexTest.kt
@@ -1,4 +1,77 @@
 package customgrammar
 
+import com.google.common.truth.Truth
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
+import ru.anton.asmirko.lexer.TokenLexer
+import ru.anton.asmirko.parser.parser.RegexParser
+import ru.anton.asmirko.parser.utils.initGrammarFromString
+import ru.anton.asmirko.testutills.getTerminalTokens
+import ru.anton.asmirko.testutills.readFromUrl
+import ru.anton.asmirko.testutills.resourceURLLoader
+
 class RegexTest {
+
+    @CsvSource(
+        value = [
+            "abacaba, abacaba",
+            "(a), (a)",
+            "a, a",
+            "a(b), a(b)",
+            "(ab), (ab)",
+            "a|(ab), a|(ab)",
+            "a*, a*",
+            "a, a",
+            "a*|ab|(cd|ef), a*|ab|(cd|ef)",
+            "a*|b*, a*|b*",
+            "a*b*c*d, a*b*c*d",
+            "a|b|c|d, a|b|c|d",
+            "abcd*ef, abcd*ef",
+            "abcd(e)fghi, abcd(e)fghi",
+            "a**, a**"
+        ]
+    )
+    @ParameterizedTest
+    fun `test positive cases tokens`(input: String, expected: String) {
+        val grammarRawUrl = resourceURLLoader<ArithmeticExprTest>("customgrammar/regex/regex.txt")
+        val grammarRaw = readFromUrl(grammarRawUrl)
+        val (grammar, tokens) = initGrammarFromString(grammarRaw)
+        val lexer = TokenLexer(tokens, "$")
+        val parser = RegexParser(grammar, lexer)
+        val resultTree = parser.parse(listOf(input))
+        val result = mutableListOf<String>()
+        getTerminalTokens(resultTree, result)
+        Truth.assertThat(result.joinToString(separator = "")).isEqualTo(expected)
+    }
+
+    @ValueSource(
+        strings = [
+            "*",
+            "*a",
+            "|",
+            "a(",
+            "a)",
+            "(a",
+            ")a",
+            "(*)(*)",
+            "(|)",
+            "|a"
+        ]
+    )
+    @ParameterizedTest
+    fun `test negative cases`(input: String) {
+        try {
+            val grammarRawUrl = resourceURLLoader<ArithmeticLangTest>("customgrammar/regex/regex.txt")
+            val grammarRaw = readFromUrl(grammarRawUrl)
+            val (grammar, tokens) = initGrammarFromString(grammarRaw)
+            val lexer = TokenLexer(tokens, "$")
+            val parser = RegexParser(grammar, lexer)
+            parser.parse(listOf(input))
+            Assertions.fail()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
